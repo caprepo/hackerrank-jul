f42076fcd2e55cdb5b436860e49f715533ddc67c
caprepo <caprepo@users.noreply.github.com>
2020-07-22 18:21:44 +0530

    Create SortTheevent.java

---
+++ SortTheevent.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'getEventsOrder' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING team1
+     *  2. STRING team2
+     *  3. STRING_ARRAY events1
+     *  4. STRING_ARRAY events2
+     */
+
+    public static List<String> getEventsOrder(String team1, String team2, List<String> events1, List<String> events2) {
+    // Write your code here
+
+    }
+
+}
